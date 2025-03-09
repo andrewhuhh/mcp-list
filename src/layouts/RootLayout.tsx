b0d0ef0483bda0e50b3979c7074ad20b11873638
andrewhuhh <ananhuang1@gmail.com>
2025-03-09 05:04:37 -0700

    more blurrry!!

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -64,7 +64,7 @@ export function RootLayout() {
             />
           </svg>
         </div>
-        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-xl supports-[backdrop-filter]:bg-background/60">
           <div className="container flex h-14 max-w-7xl mx-auto items-center">
             <div className="flex-1 flex items-center">
               <Link to="/" className="flex items-center space-x-2">
